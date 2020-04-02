2ee20f66643b173d37a46bc9e547b5dc5ec97bf4
bragayuri <rochabragayuri@gmail.com>
2020-04-02 17:25:26 +0100

    #sigin

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+//Path is a module to help us to get the directory path
+const path = require("path");
+//To be able to serve static files such CSS and fonts.
+app.use(express.static("public"));
+
+//Constant to use ESJ files
+const esj = require("esj");
+app.set("view engine", "esj");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
+
+app.get("/sign", (req, res) => {
+  res.render("signin");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
