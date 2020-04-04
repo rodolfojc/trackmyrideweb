f542a636ea156a51724ae155cb00c9972d06fe2e
bragayuri <rochabragayuri@gmail.com>
2020-04-04 15:05:53 +0100

    sigin(fixed)

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const app = express();
 //Path is a module to help us to get the directory path
 const path = require("path");
 //To be able to serve static files such CSS and fonts.
-app.use(express.static("public"));
 
-//Constant to use ESJ files
-const esj = require("esj");
-app.set("view engine", "esj");
+// Constant to receive EJS module ( To install server side)
+const ejs = require("ejs");
+// Constant to require mongoose to be used and to communicate with the Mongo Server (Install Server Side)
+const mongoose = require("mongoose");
+
+//Body-Parse parses incoming request bodies in a middleware and make the form data available under req.body property.
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Connection with Mongo
+
+mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -19,9 +34,16 @@ app.listen(3000, () => {
 });
 
 app.get("/sign", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, "public/pages/signin.html"));
   res.render("signin");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.sendFile(path.resolve(__dirname, "about.html"));
+});
+
+//#############################################################################################//
+app.post("/sign/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+function login() {
+  var email = document.getElementById("username-field");
+  var password = document.getElementById("password-field");
+}
