e49cab28bebf00062b00845c2b2e96aacc6877bb
jadyms <33762825+jadyms@users.noreply.github.com>
2020-04-12 15:03:10 +0100

    Adding bootstrap to CSS and Headers

--- public/js/map.js
+++ public/js/map.js
@@ -1,22 +1,22 @@
-const axios =  require('axios');
+// const axios =  require('axios');
 
-const geoJSON2 = async () => {
-    let response = null;
+// const geoJSON2 = async () => {
+//     let response = null;
 
-    try{
-        response = await axios({
-        method: "GET",
-        url: "http://34.247.183.192:3000/getracks",
-        headers: {}, 
-        data: {}
-      });
-        //console.log(response);
-      }catch (err) {
-        console.log(err.message)
-      }
+//     try{
+//         response = await axios({
+//         method: "GET",
+//         url: "http://34.247.183.192:3000/getracks",
+//         headers: {}, 
+//         data: {}
+//       });
+//         //console.log(response);
+//       }catch (err) {
+//         console.log(err.message)
+//       }
 
-    return response;
-};
+//     return response;
+// };
 
 // Main properties of a map
 //set the map in Dublin City Centre
@@ -33,86 +33,86 @@ var marker = L.marker([53.347557, -6.259317]).addTo(mymap);
 //add pop up to the marker
 marker.bindPopup("<b>numberOfStands: 8</b><br>type: Sheffield Stand").openPopup();
 
-var geoJson = geoJSON2();
-// var geoJson = {
-//     type: "FeatureCollection",
-//     features: 
-//     [
-//         {
-//             type: "Feature",
-//             geometry: 
-//             {
-//             type: "Point",
-//             coordinates: 
-//             [
-//             -6.243576,
-//             53.346026
-//             ]
-//             },
-//             properties: 
-//             {
-//             checked: true,
-//             id: "557990",
-//             name: "Lime Street",
-//             notes: "",
-//             numberOfStands: 40,
-//             source: "DublinBikes",
-//             thefts: [ ],
-//             type: "DublinBikes",
-//             verified: true
-//             }
-//             },
-//             {
-//             type: "Feature",
-//             geometry: 
-//             {
-//             type: "Point",
-//             coordinates: 
-//             [
-//             -6.260177,
-//             53.330662
-//             ]
-//             },
-//             properties: 
-//             {
-//             checked: true,
-//             id: "c6da28",
-//             name: "Charlemont Street",
-//             notes: "",
-//             numberOfStands: 40,
-//             source: "DublinBikes",
-//             thefts: [ ],
-//             type: "DublinBikes",
-//             verified: true
-//             }
-//             },
-//             {
-//             type: "Feature",
-//             geometry: 
-//             {
-//             type: "Point",
-//             coordinates: 
-//             [
-//             -6.310015,
-//             53.342113
-//             ]
-//             },
-//             properties: 
-//             {
-//             checked: true,
-//             id: "91cc6b",
-//             name: "Kilmainham Gaol",
-//             notes: "",
-//             numberOfStands: 40,
-//             source: "DublinBikes",
-//             thefts: [ ],
-//             type: "DublinBikes",
-//             verified: true
-//             }
-//             },
+// var geoJson = geoJSON2();
+var geoJson = {
+    type: "FeatureCollection",
+    features: 
+    [
+        {
+            type: "Feature",
+            geometry: 
+            {
+            type: "Point",
+            coordinates: 
+            [
+            -6.243576,
+            53.346026
+            ]
+            },
+            properties: 
+            {
+            checked: true,
+            id: "557990",
+            name: "Lime Street",
+            notes: "",
+            numberOfStands: 40,
+            source: "DublinBikes",
+            thefts: [ ],
+            type: "DublinBikes",
+            verified: true
+            }
+            },
+            {
+            type: "Feature",
+            geometry: 
+            {
+            type: "Point",
+            coordinates: 
+            [
+            -6.260177,
+            53.330662
+            ]
+            },
+            properties: 
+            {
+            checked: true,
+            id: "c6da28",
+            name: "Charlemont Street",
+            notes: "",
+            numberOfStands: 40,
+            source: "DublinBikes",
+            thefts: [ ],
+            type: "DublinBikes",
+            verified: true
+            }
+            },
+            {
+            type: "Feature",
+            geometry: 
+            {
+            type: "Point",
+            coordinates: 
+            [
+            -6.310015,
+            53.342113
+            ]
+            },
+            properties: 
+            {
+            checked: true,
+            id: "91cc6b",
+            name: "Kilmainham Gaol",
+            notes: "",
+            numberOfStands: 40,
+            source: "DublinBikes",
+            thefts: [ ],
+            type: "DublinBikes",
+            verified: true
+            }
+            },
             
-// ]
-// }
+]
+}
 
    
 
@@ -122,7 +122,7 @@ var geoJson = geoJSON2();
    var popup = L.popup();
    
     //Create a personalized marker
-   var geojsonMarkerOptions = {
+   var hotspotMarker = {
     radius: 8,
     fillColor: "#ff7800",
     color: "#000",
@@ -130,6 +130,13 @@ var geoJson = geoJSON2();
     opacity: 1,
     fillOpacity: 0.8
 };
+
+var rackMarker = L.icon({
+    iconUrl: 'img/bikelogo.png',
+    iconSize:     [40, 60], // size of the icon
+    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 //new layer, could be used for nightime map
 var difflayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -142,24 +149,48 @@ function onEachFeature(feature, layer){
 }
 
     
-    var mapWithMarkers = L.geoJSON(geoJson, {
+    var mapWithRackMarkers = L.geoJSON(geoJson, {
         //add multiple locations with personalised marker using geoJson
         pointToLayer: function(feature, latlng){
-            return L.circleMarker(latlng, geojsonMarkerOptions);
+           return L.marker(latlng, {
+               icon: rackMarker
+           });
+           
+            // return L.circleMarker(latlng, geojsonMarkerOptions);
+           },
+
+        onEachFeature: onEachFeature,
+    }).addTo(mymap);
+
+    var mapWithHotspotsMarkers = L.geoJSON(geoJson, {
+        //add multiple locations with personalised marker using geoJson
+        pointToLayer: function(feature, latlng){
+             return L.circleMarker(latlng, hotspotMarker);
            },
 
         onEachFeature: onEachFeature,
     }).addTo(mymap);
 
 
-    //When button is clicked, toggle the markers
+    //When button is clicked, toggle the Racks markers 
     $(document).on('click', '#showRacks', function() {
         //If markers are visible, hide it
-        if(mymap.hasLayer(mapWithMarkers)){
-            mymap.removeLayer(mapWithMarkers);
+        if(mymap.hasLayer(mapWithRackMarkers)){
+            mymap.removeLayer(mapWithRackMarkers);
         }else{
             //If markers are hidden, show them
-            mapWithMarkers.addTo(mymap);
+            mapWithRackMarkers.addTo(mymap);
+        }
+    });
+
+    //When button is clicked, toggle the Hotspots markers 
+    $(document).on('click', '#showHotspots', function() {
+        //If markers are visible, hide it
+        if(mymap.hasLayer(mapWithHotspotsMarkers)){
+            mymap.removeLayer(mapWithHotspotsMarkers);
+        }else{
+            //If markers are hidden, show them
+            mapWithHotspotsMarkers.addTo(mymap);
         }
     });
 
