ae8700fb861acf6e0eec8264d55b12c0bca1bcb9
jadyms <33762825+jadyms@users.noreply.github.com>
2020-04-15 11:36:05 +0100

    SideBar Development
    If successful, delete copy of files:
    -map.js
    -header.ejs
    -map.ejs
    -style.css

--- public/js/map copy.js
+++ public/js/map copy.js
@@ -267,14 +267,8 @@ L.control.layers({
             theme: "minimal"
         });
 
-        $('#dismiss, .overlay').on('click', function () {
-            $('#sidebar').removeClass('active');
-            $('.overlay').removeClass('active');
-        });
-
         $('#chatToggle').on('click', function () {
-            $('#sidebar').addClass('active');
-            $('.overlay').addClass('active');
+            $('#sidebar, #content').toggleClass('active');
             $('.collapse.in').toggleClass('in');
             $('a[aria-expanded=true]').attr('aria-expanded', 'false');
         });
