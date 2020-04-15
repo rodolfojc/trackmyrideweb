5c465296e526404505aa27ec677eb73396625d0e
bragayuri <rochabragayuri@gmail.com>
2020-04-15 13:15:59 +0100

    MVC-refactoring and routes

--- models/User.js
+++ models/User.js
@@ -1,9 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
-  email: String,
-  password1: String,
+  //Mongoose validation before saving it to the Database.
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password1: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  bcrypt.hash(user.password1, 10, (error, hash) => {
+    user.password = hash;
+    next();
+  });
 });
 
 const User = mongoose.model("User", UserSchema);
