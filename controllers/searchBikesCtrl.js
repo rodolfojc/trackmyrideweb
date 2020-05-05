15065934bd918dfded3424d494ac4b4abfe44b5a
jadyms <33762825+jadyms@users.noreply.github.com>
2020-05-05 21:04:15 +0100

    Display bike by serial

--- controllers/searchBikesCtrl.js
+++ controllers/searchBikesCtrl.js
@@ -2,9 +2,11 @@ const bikeModel = require("../models/Bike.js");
 
 module.exports = async (req, res) => {
     const id = req.body.serial;
-    const bikeInfoPage = await bikeModel.findOne({serial: id}) ;    
+    const bikeInfoPage = await bikeModel.find({});
     
-        res.send(
-            bikeInfoPage,
-        );
+    // const bikeInfoPage = await bikeModel.findOne({serial: id}) ;    
+    console.log(bikeInfoPage);
+    console.log(typeof bikeInfoPage);
+    res.render("bikeResults",  {bikeInfoPage});
+    // res.render("bikeResults",  {bikeInfoPage: bikeInfoPage});
 }
