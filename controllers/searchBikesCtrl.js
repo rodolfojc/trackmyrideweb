96e49641e63badcd2330800b70952c43ccefdf4c
jadyms <33762825+jadyms@users.noreply.github.com>
2020-05-11 22:43:39 +0100

    Catching when no bike is found by serial

--- controllers/searchBikesCtrl.js
+++ controllers/searchBikesCtrl.js
@@ -3,10 +3,24 @@ const bikeModel = require("../models/Bike.js");
 module.exports = async (req, res) => {
     const id = req.body.serial;
   //  const bikeInfoPage = await bikeModel.find({});
+  try {
     
-    const bikeInfoPage = await bikeModel.findOne({serial: id}) ;    
-    console.log(bikeInfoPage);
-    console.log(typeof bikeInfoPage);
-    res.render("bikeResults",  {bikeInfoPage});
+    const bikeInfoPage = await bikeModel.findOne({serial: id});  
+    if(!bikeInfoPage) {
+      throw new Error('no document found');
+    }
+      res.render("bikeResults",  {bikeInfoPage});
+      return bikeInfoPage;
+    } catch (error) {
+    console.log(`findOne error--> ${error}`);
+    res.redirect('/bikeinfo');
+    return error;
+    
+}
+    
+  //res.render("bikeResults",  {bikeInfoPage});
+    //console.log(bikeInfoPage);
+    //console.log(typeof bikeInfoPage);
+    //res.render("bikeResults",  {bikeInfoPage});
     // res.render("bikeResults",  {bikeInfoPage: bikeInfoPage});
 }
