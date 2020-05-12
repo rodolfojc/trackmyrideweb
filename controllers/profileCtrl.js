b9a1010ebbae8905b7adb526f27cb3f7b78d8390
jadyms <33762825+jadyms@users.noreply.github.com>
2020-05-12 22:39:43 +0100

    Passing user id from model to profile

--- controllers/profileCtrl.js
+++ controllers/profileCtrl.js
@@ -1,12 +1,36 @@
 const bikeModel = require("../models/Bike.js");
 
+
 module.exports = async (req, res) => {
-    //const id = req.params.id;
-    const bikeInfoPage = await bikeModel.find({});
+    // const id =  mongoose.Types.ObjectId(req.session.userId);
+     const id = req.session.userId;
+    console.log(id);
+
+  try {
     
-    //const success = await bikeModel.findByIdAndUpdate(id, {$push: {report: req.body.description}}, {new: true}) ;    
- 
-    //res.send(success);
-   res.render("profile");
+    const userBikeDetails = await bikeModel.findOne({userId: id}); 
+    if(!userBikeDetails) {
+
+      throw new Error('User has no data registered');
+      
+    }
+    console.log(userBikeDetails);
+      res.render("profile",  {userBikeDetails: userBikeDetails});
+      return userBikeDetails;
+    } catch (error) {
+    console.log(`findOne error--> ${error}`);
+  
+    res.render("profile", {noData : true} ); 
+  
+    return error;
+    
+}
+//receive a tru or false if no data is in the profile
+  //Find the bikes this user id has and render to the profile page
+
+  //Find the reports this user id has and render to the profile page
+
+
+ //  res.render("profile", { userId: userId });
    
 }
--- models/Bike.js
+++ models/Bike.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const BikeSchema = new Schema({
 	// serial: String,
+	userId: {type: mongoose.Schema.Types.ObjectId},
 	serial: Number, //The db is number, here was String - Jady
 	brand: String,
 	color: String,
