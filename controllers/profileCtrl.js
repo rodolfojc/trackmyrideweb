8b19d2499e32ca4fbba22184f6e47ed9b74fafa9
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-05-18 20:23:13 +0100

    profile pic minor

--- controllers/profileCtrl.js
+++ controllers/profileCtrl.js
@@ -30,10 +30,11 @@ const axios = require('axios');
             accountDetails = result[0];    //Assign user object
              userBikeDetails = result[1]; //Assign bikes object
              bikeCount = result[2];
-             profilePic = result[3];
+             profilePic = result[3].url;
          
             //Display profile page and send both user and bike objects
             console.log(userId);
+            console.log(profilePic);
             console.log(userBikeDetails);
             console.log(accountDetails);
             res.render("profile",  {userBikeDetails: userBikeDetails, accountDetails: accountDetails, bikeCount:bikeCount, userId:userId, profilePic: profilePic})
